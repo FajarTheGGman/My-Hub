9551da446e652e595bc7ec4ae29e3f426a805dd3
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-03-30 19:24:59 +0700

    Add files via upload

---
+++ My-Hub.cpp
@@ -0,0 +1,259 @@
+//Please Don't Recode My program
+//Copyright© 2019 By Fajar Firdaus
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "    __  ___            __  __      __\n";
+cout << "   /  |/  /_  __      / / / /_  __/ /_\n";
+cout << "  / /|_/ / / / /_____/ /_/ / / / / __ \n";
+cout << " / /  / / /_/ /_____/ __  / /_/ / /_/ /\n";
+cout << "/_/  /_/  _, /     /_/ /_/  _,_/_.  _/\n";
+cout << "       /____/\n\n";
+cout << "";
+cout << "[=====================]\n";
+cout << "Author : Fajar Firdaus\n";
+cout << "FB : Fajar Firdaus\n";
+cout << "IG : fajar_firdaus_7\n";
+cout << "YT : iTech7732\n";
+cout << "[=====================]\n\n";
+cout << "[======================]\n";
+cout << "(1).AdminSearch\n";
+cout << "(2).UploadFinder\n";
+cout << "(3).MatthewBot\n";
+cout << "(4).Cydia\n";
+cout << "(5).SpamJS\n";
+cout << "(6).F-Tools\n";
+cout << "(7).Login_Session\n";
+cout << "(8).Termux-GUI\n";
+cout << "(9).MetaInstaller\n";
+cout << "(10).NetScan\n";
+cout << "(11).FakeCall\n";
+cout << "(12).F-Hub\n";
+cout << "(13).LazadaChecker\n";
+cout << "(14).HashCracker\n";
+cout << "(15).IGINFORM\n";
+cout << "(16).Iptracker\n";
+cout << "(17).F-Dos\n";
+cout << "(18).ProxyPicker\n";
+cout << "(19).OpenHash\n";
+cout << "(20).IslamKiblat\n";
+cout << "(21).IslamPray\n";
+cout << "(22).ScDeface323\n";
+cout << "(23).IGStalker\n";
+cout << "(24).HashMaker\n";
+cout << "(25).Gitinform\n";
+cout << "(26).XSploit\n";
+cout << "(27).NetWorkSpoofing\n";
+cout << "(28).MetaBot\n";
+cout << "(29).Crsf-file-upload-exploiter\n";
+cout << "(30).PasTen\n";
+cout << "(31).WLoad\n";
+cout << "(32).DefaceTools\n";
+cout << "(33).CekCuaca\n";
+cout << "(34).TheToxicBot\n";
+cout << "(35).FuckingPentest\n";
+cout << "(36).BugMaker\n";
+cout << "(37).WebDavExploiter\n";
+cout << "(38).Sqlbot\n";
+cout << "[======================]\n";
+int f;
+cout << "root@F-Git-# ";
+cin >> f;
+if (f <= 1){
+cout << "Installing....";
+system("git clone https://github.com/FajarTheGGman/AdminSearch");
+system("cd AdminSearch");
+system("sh AdminSearch/install");
+}else if (f <= 2){
+	cout << "Installing...";
+	system("git clone https://github.com/FajarTheGGman/UploadFinder");
+	system("cd UploadFinder");
+	system("apt-get install nodejs -y");
+	system("npm install --save colors request readline fs exit");
+	system("node UploadFinder/upload.ts");
+}else if (f <= 3){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/MatthewBot");
+	system("cd MatthewBot");
+	system("apt-get install php -y");
+	system("php MatthewBot/Matthew.php");
+}else if (f <= 4){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/Cydia");
+	system("cd Cydia");
+	system("sh Cydia/install");
+}else if (f <= 5){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/SpamJS");
+	system("cd SpamJS");
+	system("sh SpamJS/install");
+}else if (f <= 6){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/F-Tools");
+	system("cd F-Tools");
+	system("sh F-Tools/F-Tools.sh");
+}else if (f <= 7){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/Login_Session");
+	system("cd Login_Session");
+	system("ls");
+}else if (f <= 8){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/Termux-GUI");
+	system("cd Termux-GUI");
+	system("sh install");
+	system("npm install colors shelljs");
+	system("node Termux-GUI/fbox.js");
+}else if (f <= 9){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/MetaInstaller");
+	system("cd MetaInstaller");
+	system("apt-get install nodejs -y");
+	system("npm install --save colors shelljs");
+	system("node MetaInstaller/meta.js");
+}else if (f <= 10){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/NetScan");
+	system("cd NetScan");
+	system("sh NetScan/install.sh");
+}else if (f <= 11){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/FakeCall");
+	system("cd FakeCall");
+	system("apt-get install php -y");
+	system("php FakeCall/spam.php");
+}else if (f <= 12){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/F-Hub");
+	system("cd F-Hub");
+	system("apt-get install php -y");
+	system("php F-Hub/F-Hub.php");
+}else if (f <= 13){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/LazadaChecker");
+	system("cd LazadaChecker");
+	system("sh LazadaChecker/install");
+	system("node LazadaChecker/lazada");
+}else if (f <= 14){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/HashCracker");
+	system("cd HashCracker");
+	system("sh HashCracker/install");
+	system("node HashCracker/hash");
+}else if (f <= 15){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/IGINFORM");
+	system("cd IGINFORM");
+	system("sh IGINFORM/install");
+	system("node IGINFORM/ig.js");
+}else if (f <= 16){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/IpTracker");
+	system("cd Iptracker");
+	system("apt-get install php -y");
+	system("php Iptracker/ip.php");
+}else if (f <= 17){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/F-Dos");
+	system("cd F-Dos");
+	system("apt-get install python2");
+	system("python2 F-Dos/F-Dos.py");
+}else if (f <= 18){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/ProxyPicker");
+	system("cd ProxyPicker");
+	system("apt-get install php -y");
+	system("php ProxyPicker/proxypicker.php");
+}else if (f <= 19){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/OpenHash");
+	system("cd OpenHash");
+	system("apt-get install php -y");
+	system("php OpenHash/openhash.php");
+}else if (f <= 20){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/IslamKiblat");
+	system("cd IslamKiblat");
+	system("apt-get install php -y");
+	system("php Islam/islamkiblat.php");
+}else if (f <= 21){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/IslamPray");
+	system("apt-get install php -y");
+	system("php IslamPray/islam.php");
+}else if (f <= 22){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/ScDeface323");
+    system("cd ScDeface323");
+    system("ls");
+}else if (f <= 23){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/IGStalker");
+	system("apt-get install php -y");
+	system("php IGStalker/ig.php");
+}else if (f <= 24){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/HashMaker");
+	system("sh HashMaker/hashmaker.sh");
+}else if (f <= 25){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/GitInform");
+	system("apt-get install php -y");
+	system("php Gitinform/gitinform.php");
+}else if (f <= 26){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/XSploit");
+	system("sh XSploit/xsploit.sh");
+}else if (f <= 27){
+    cout << "Installing....";
+    system("git clone https://github.com/FajarTheGGman/NetWorkSpoofing");
+    system("sh NetWorkSpoofing/NetWorkSpoofing.sh");
+}else if (f <= 28){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/MetaBot");
+	system("sh MetaBot/metabot.sh");
+}else if (f <= 29){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/Csrf-file-upload-exploiter");
+	system("ls Crsf-file-upload-exploiter/");
+}else if (f <= 30){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/PasTen");
+	system("sh PasTen/pasten.sh");
+}else if (f <= 31){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/WLoad");
+	system("sh WLoad/WLoad.sh");
+}else if (f <= 32){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/DefaceTools");
+	system("sh DefaceTools/defacetools.sh");
+}else if (f <= 33){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/CekCuaca");
+	system("sh CekCuaca/cuaca.sh");
+}else if (f <= 34){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/TheToxicBot");
+	system("apt-get install php -y");
+	system("php TheToxicBot/thetoxicbot.php");
+}else if (f <= 35){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/FuckingPentest");
+	system("sh FuckingPentest/fuckingpentest.sh");
+}else if (f <= 36){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/BugMaker");
+	system("sh BugMaker/bugmaker.sh");
+}else if (f <= 37){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/WebDavExploiter");
+	system("sh WebDavExploiter/webdavexploiter.sh");
+}else if (f <= 38){
+	cout << "Installing....";
+	system("git clone https://github.com/FajarTheGGman/Sqlbot");
+	system("sh Sqlbot/sqlbot.sh");
+}
+}
